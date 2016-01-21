8532fce8f63b0fb0c4562b086300e196ff7e2862
Justin Drew <jdrew@sca-corp.com>
2016-01-21 08:45:22 -0500

    bunch of changes

--- support/js/main.js
+++ support/js/main.js
@@ -1,8 +1,15 @@
-function headerBtnClick (button) {
-	var target = $('#' + button.id + "Content").get (0);
-	var parent = $(target.parentNode).get (0);
-	
-	var trueOffset = $(target).prop ('offsetTop') - $(parent).prop('offsetTop');
-	
-	$( "#" +parent.id).animate({scrollTop: trueOffset});
+function header_button_click(button) {
+	var id = '#' + $(button).prop('id') + 'Content',
+	    parentId = '#' + $(id).parent().prop('id'),
+	    trueOffset = $(id).prop ('offsetTop') - $(parentId).prop('offsetTop');
+
+	$($(button).parent()).children().each(function(i, e) {
+		if($(e).prop('tagName').toLowerCase() === 'button') {
+			$(e).removeClass('active');
+		}
+	});
+
+	$(button).addClass('active');
+
+	$($(id).parent()).animate({scrollTop: trueOffset});
 }
